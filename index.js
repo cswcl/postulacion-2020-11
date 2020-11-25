d7bb6d8d9541754e3e812d411aff35b696f276ce
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2020-11-25 15:51:59 -0300

    Add package.json, test script and starting point file

---
+++ index.js
@@ -0,0 +1,3 @@
+// Escribe tu código aquí así los scripts de npm siguen funcionando
+// (No renombres este archivo por favor)
+
---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+node index.js < 0.in >| /tmp/0.out
+node index.js < 1.in >| /tmp/1.out
+node index.js < 2.in >| /tmp/2.out
+
+cd /tmp
+md5sum 0.out >| /tmp/md5
+md5sum 1.out >> /tmp/md5
+md5sum 2.out >> /tmp/md5
+
+cd -
+md5sum -c /tmp/md5
